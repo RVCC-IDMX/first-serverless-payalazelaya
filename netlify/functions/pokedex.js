a52d353e119faf70ee045f43ac98c82fb0c67794
Paola Ayala <paolaaz235@gmail.com>
2022-04-23 15:13:33 -0400

    Finished Serverless

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,23 @@
 // mod.cjs
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
 
 exports.handler = async function (event, context) {
+  const date = DateTime.now();
+
   const eventBody = JSON.parse(event.body);
+  console.log(chalk.cyan(`${date}`));
+  console.log(chalk.green(`\teventBody.region: ${eventBody.region}`));
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
 
   const response = await fetch(POKE_API);
   const data = await response.json();
+
+  console.log(
+    chalk.blue(`\tNumber of entries: ${datat.pokemon_entries.length}`)
+  );
 
   return {
     statusCode: 200,
